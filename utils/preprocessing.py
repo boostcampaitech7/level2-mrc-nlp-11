d380c0ce745ab0b6652b41f784ac2aa53fb7bf4c
CLM-BONNY <alstjs4739@kookmin.ac.kr>
2024-10-12 20:45:44 +0000

    feat: 마크다운 패턴 제거 함수(remove_markdown) answer_start 계산 추가 구현
    #11

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -13,6 +13,7 @@ STANDARD DATA FORMAT
 """
 
 import re
+from transformers import BertTokenizerFast
 
 
 def test(example):
@@ -33,6 +34,22 @@ def title_context_merge(example):
 
 # 마크다운 패턴 제거 함수
 def remove_markdown(example):
+    # 원본 문맥과 answer_start 저장
+    original_context = example["context"]
+    original_answer_start = example["answers"]["answer_start"][0]
+
+    tokenizer = BertTokenizerFast.from_pretrained("klue/bert-base")
+
+    # 원본 문맥을 토큰화
+    original_tokens = tokenizer(original_context)["input_ids"]
+    print(original_tokens[226])
+
+    # 답변 시작 텍스트 토큰화 결과 저장
+    answer_token = original_tokens[original_answer_start]
+
+    # 답변 시작 토큰이 같은 토큰값 중 몇 번째 인덱스에 위치하는지 저장
+    original_index = find_target_index(original_tokens, answer_token)
+
     # 마크다운 패턴 리스트를 함수 내부에 포함
     markdown_patterns = {
         "header": r"(?:^|\n{1,2})#{1,6}\s*",  # 헤더 (1~2번의 줄바꿈 허용)
@@ -44,6 +61,19 @@ def remove_markdown(example):
         example["context"] = re.sub(
             pattern, " ", example["context"], flags=re.MULTILINE
         )
+
+    # 마크다운을 제거한 후 새로운 문맥을 토큰화
+    new_tokens = tokenizer(example["context"])["input_ids"]
+    print(new_tokens[223])
+
+    # 답변 시작 토큰이 같은 토큰값의 인덱스들을 찾음
+    new_indices = [i for i, token in enumerate(new_tokens) if token == answer_token]
+
+    # original_index번째 값을 example['answers']['answer_start']에 저장
+    if original_index < len(new_indices):
+        example["answers"]["answer_start"] = new_indices[original_index]
+    else:
+        example["answers"]["answer_start"] = None  # 또는 예외 처리
 
     return example
 
