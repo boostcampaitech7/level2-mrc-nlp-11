3ee967e7d7b288999f254ba56c334e7880fe63a0
jhyeop <pizzauniv99@gmail.com>
2024-10-16 18:35:52 +0900

    feat: openai API로 원본 데이터셋의 질문 paraphrasing하는 함수 구현
    #37

---
+++ utils/modify_questions.py
@@ -0,0 +1,66 @@
+import json
+from datasets import load_from_disk
+from dotenv import load_dotenv
+import os
+import asyncio
+from call_openai_API import call_openai_API
+
+
+def modify_questions(questions, mode):
+
+    prompt_content = f"""
+        다음은 질문 목록입니다. 각 질문 문장의 의미를 유지한채로 몇몇 단어를 동의어로 변경하여 패러프레이징해주세요.
+        ex)
+        - 올레 폰 보이스트 이후에 시장으로 임명된 사람은 누구인가? -> 올레 폰 보이스트 다음에 시장으로 선임된 인물은 누구인가?
+        - 우메노의 사인은? -> 우메노가 죽은 원인은 무엇인가?
+        응답은 원래 문장을 key, 바꾼 문장을 value로 하는 순수한 JSON 형식으로 제공해주세요. 단, 바꾼 문장에 코드 블록이나 다른 텍스트는 포함되지 않도록 해주세요. 또한, 원래 문장, 바꾼 문장에 "이 포함될 경우 이스케이프 문자를 붙여 JSON 파싱 과정에 오류가 생기지 않도록 해주세요.
+        """
+
+    response_format = rf"""
+        [
+            {{"original": "올레 폰 보이스트 이후에 시장으로 임명된 사람은 누구인가?", "paraphrased": "올레 폰 보이스트 다음에 시장으로 선임된 인물은 누구인가?"}},
+            {{"original": "2007년 자유 지수 순위 참여 국가 중 꼴찌를 한 국가는?", "paraphrased": "2007년 자유 지수 순위에 참여한 국가 중 최하위를 기록한 국가는?"}},
+            ...
+        ]
+        """
+
+    response = asyncio.run(
+        call_openai_API(questions, 10, prompt_content, response_format)
+    )
+
+    paraphrased_questions = {}
+    for batch in response:
+        for paraphrased_question in batch:
+            if paraphrased_question == None:
+                continue
+            paraphrased_questions[paraphrased_question["original"]] = (
+                paraphrased_question["paraphrased"]
+            )
+
+    # save
+    output_path = (
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        + f"/data/paraphrased_{mode}_questions.json"
+    )
+    with open(output_path, "w", encoding="utf-8") as f:
+        json.dump(paraphrased_questions, f, ensure_ascii=False, indent=4)
+
+
+def main():
+    load_dotenv()
+
+    dataset_path = os.getenv("DIR_PATH") + "/level2-mrc-nlp-11/data/default"
+    dataset = load_from_disk(dataset_path)
+
+    train_dataset = dataset["train"]
+    validation_dataset = dataset["validation"]
+
+    train_questions = train_dataset["question"]
+    validation_questions = validation_dataset["question"]
+
+    modify_questions(train_questions, "train")
+    modify_questions(validation_questions, "validation")
+
+
+if __name__ == "__main__":
+    main()
